6d1e700f95fb19734aafb472f7be9113cc1cbd47
Timur-Gus <timurka24478@gmail.ru>
2024-06-02 16:52:23 +0300

    Готовое решение.

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -8,6 +8,7 @@ import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.model.NotificationTask;
 import pro.sky.telegrambot.repository.NotificationTaskRepository;
@@ -15,6 +16,7 @@ import pro.sky.telegrambot.repository.NotificationTaskRepository;
 import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -68,4 +70,23 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void sendingNotification() {
+        List<NotificationTask> notificationTasks = notificationTaskRepository.findAll();
+        LocalDateTime dateTime = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+        for (NotificationTask notificationTask : notificationTasks) {
+            if (notificationTask.getDataTime().equals(dateTime)) {
+                SendMessage message = new SendMessage(
+                        notificationTask.getChatId(),
+                        notificationTask.getTextNotification());
+                SendResponse response = telegramBot.execute(message);
+            }
+        }
+
+        }
+
 }
+
+
+
+
